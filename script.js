0ca414a7e833c48db4efc66dd71a7f5d29784934
Celeste Layne <layne.celeste@gmail.com>
2015-11-05 10:34:19 -0800

    add setTimeout to changeLeftColor

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // STRUCTURE
-	var form = document.querySelectorAll('form');
 
 	var leftInput = document.querySelector('.left-input');
 	var rightInput = document.querySelector('.right-input');
@@ -20,36 +19,35 @@
 		event.preventDefault();
 		// Farenheit to Celsius
 		// console.log(event) <- keyboard event
-		var FtoC = (leftInput.value - 32) * (5/9);
-		rightInput.value = Math.round(FtoC);
-		var celsius = rightInput.value;
+			var FtoC = (leftInput.value - 32) * (5/9);
+			rightInput.value = Math.round(FtoC);
+			var celsius = rightInput.value;
 	}
 
 
 	function calcCelsiusToFarenheit(event){
 		event.preventDefault();
 		// Celsius to Farenheit
-		var CtoF = (rightInput.value * (9/5)) + 32;
-		leftInput.value = Math.round(CtoF);
-		var farenheit = leftInput.value;
-		// console.log(farenheit);
+			var CtoF = (rightInput.value * (9/5)) + 32;
+			leftInput.value = Math.round(CtoF);
+			var farenheit = leftInput.value;			
 	}
 
 	function changeRightColor(farenheit){
-		console.log(event.target.value);
+		// console.log(event.target.value);
 			if (event.target.value <= 32){
 				leftBody.classList.add('cold');
 				rightBody.classList.add('cold-animation');
 			} else {
 				setTimeout(function() {
 					rightBody.classList.remove('cold-animation');
-				}), 400} if (event.target.value > 32 && event.target.value <= 212){
+				}), 400} if (event.target.value > 32 && event.target.value <= 86){
 				leftBody.classList.add('warm');
 				rightBody.classList.add('warm-animation');
 			} else {
 				setTimeout(function() {
 					rightBody.classList.remove('warm-animation');
-				}), 400} if (event.target.value > 212){
+				}), 400} if (event.target.value > 86){
 				leftBody.classList.add('hot');
 				rightBody.classList.add('hot-animation');
 			} else {
@@ -60,17 +58,23 @@
 
 	function changeLeftColor(celsius) {
 
-		console.log(celsius);
 		if (event.target.value <= 0) {
 			rightBody.classList.add('cold');
 			leftBody.classList.add('cold-animation');
-		} else if (event.target.value > 0 && event.target.value <= 30){
+		} else {
+			setTimeout(function(){
+				leftBody.classList.remove('cold-animation');
+			}), 400} if (event.target.value > 0 && event.target.value <= 30){
 			rightBody.classList.add('warm');
 			leftBody.classList.add('warm-animation');
-		} else if (event.target.value > 30){
+		} else {
+			setTimeout(function(){
+				leftBody.classList.remove('warm-animation');
+			}), 400} if (event.target.value > 30){
 			rightBody.classList.add('hot');
 			leftBody.classList.add('hot-animation');
 		} else {
-			return ' ';
-		}
+			setTimeout(function(){
+				leftBody.classList.remove('hot-animation');
+			}), 400}
 	}
